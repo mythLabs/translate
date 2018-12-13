0591aa117d402bb212d6c78936c2698de9685f0e
mythLabs <amitbisht744@gmail.com>
2018-12-13 20:47:33 +0530

    refactor language selection to seperate component

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,19 @@ import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../context/LanguageContext";
 import ColorContext from '../context/ColorContext';
+import LanguageSelector from './LanguageSelector'
 
 class App extends React.Component {
   state = { language: "english" };
 
-  onLanguagechange = language => {
+  onLanguageChange = language => {
     this.setState({ language });
   };
 
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguagechange("english")}
-          />
-          <i
-            className="flag in"
-            onClick={() => this.onLanguagechange("hindi")}
-          />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange}></LanguageSelector>
         <LanguageContext.Provider value={this.state.language}>
          <ColorContext.Provider value={'red'}>
           <UserCreate />
